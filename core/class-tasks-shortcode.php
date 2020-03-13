0f3f15d57b8eae516fb6ae62162550a751405ef4
Antonio Blanco Oliva <blanco@Mac-mini-de-Antonio.local>
2020-03-13 21:59:16 +0100

    Added list shortcode

--- core/class-tasks-shortcode.php
+++ core/class-tasks-shortcode.php
@@ -1,49 +1,67 @@
 <?php
 class Tasks_Shortcode {
     public static function init()  {
-        add_shortcode('tasks_listado', array( __CLASS__, 'tasks_listado' ) );
+        add_shortcode('tasks_list', array( __CLASS__, 'tasks_list' ) );
     }
 
-    public static function tasks_listado() {
+    public static function tasks_list() {
         $output = '';
-        $productos = get_posts( array( 'post_type' => 'producto', 'numberposts' => -1 ) );
-        if ( $productos ) {
-            $output .= '<div class="row producto-row">';
-            foreach ( $productos as $key => $producto ) {
-                if ( ( $key % 3 ) == 0 ) {
-                    $output .= '</div>';
-                    $output .= '<div class="row producto-row">';
-                }
-                $output .= '<div class="col-sm-4">';
-                $output .= '<div class="imagen-producto-container">';
-                $output .= get_the_post_thumbnail( $producto->ID, 'medium' );
-                $output .= '</div>';
-                $output .= '<span class="price">' . get_post_meta( $producto->ID, 'precio', true ) . '</span>';
+        $tasks = get_posts( array( 'post_type' => 'task', 'numberposts' => -1 ) );
+        if ( $tasks ) {
+            $output .= '<div class="container">';
+            $output .= '<div class="row task-row">';
+            $output .= '
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Asignada a</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Estado</th>
+                    </tr>
+                </thead>
+                <tbody>';
+            foreach ( $tasks as $key => $task ) {
+                $output .= '
+                <tr>
+                  <th scope="row">#' . $task->ID . '</th>
+                  <td>' . 'Autor' . '</td>
+                  <td><a href="' . get_the_permalink( $task->ID ) . '">' . get_the_title( $task->ID ) . '</a></td>
+                  <td>' . strip_tags( get_the_term_list( $task->ID, 'state' ) ) . '</td>
+                </tr>
+                ';
+                
+                /*
+                $output .= '<div class="col-sm-12">';
                 $output .= '<div class="">';
-                $output .= '<a href="#producto_modal_' . $key . '" data-toggle="modal" data-target="#producto_modal_' . $key . '">';
-                $output .= get_the_title( $producto->ID );
+                $output .= '<a href="#task_modal_' . $key . '" data-toggle="modal" data-target="#task_modal_' . $key . '">';
+                $output .= get_the_title( $task->ID );
                 $output .= '</a>';
                 $output .= '</div>';
                 $output .= '</div>';
 
                 // Modal 
-                $output .= '<div class="modal fade" id="producto_modal_' . $key . '" tabindex="-1" role="dialog">';
+                $output .= '<div class="modal fade" id="task_modal_' . $key . '" tabindex="-1" role="dialog">';
                 $output .= '<div class="modal-dialog modal-lg">';
                 $output .= '<div class="modal-content">';
                 $output .= '<div class="modal-header">';
-                $output .= '<h5 class="modal-title">' . get_the_title( $producto->ID ) . '</h5>';
+                $output .= '<h5 class="modal-title">' . get_the_title( $task->ID ) . '</h5>';
                 $output .= '</div>';
                 $output .= '<div class="modal-body">';
-                $output .= get_post_field('post_content', $producto->ID );
+                $output .= get_post_field('post_content', $task->ID );
                 $output .= '</div>';
                 $output .= '</div>';
                 $output .= '</div>';
                 $output .= '</div>';
+                */
             }
-            $output .= '</div>';
-            $output .= '</div>';
+            //$output .= '</div>';
+            $output .= '
+              </tbody>
+            </table>
+            ';
         } else {
-            $output .= '<h2>En breve podra ver aquí nuestra sección de productos</h2>';
+            $output .= '<h2>En breve podra ver aquí nuestra sección de tasks</h2>';
         }
         return $output;
     }
